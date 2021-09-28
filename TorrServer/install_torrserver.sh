0733c679421477b0871722953fa509835d69439d
nickwild93 <91117443+nickwild93@users.noreply.github.com>
2021-09-28 13:57:59 +0300

    Update install_torrserver.sh

--- TorrServer/install_torrserver.sh
+++ TorrServer/install_torrserver.sh
@@ -4,7 +4,7 @@ mkdir /opt/torrserver
 mv TorrServer-linux-amd64 /opt/torrserver
 chmod +x /opt/torrserver/TorrServer-linux-amd64
 apt -y install systemd-container
-wget https://github.com/nickwild93/cookbook/blob/main/TorrServer/torrserver.service -O /etc/systemd/system/
+wget https://raw.githubusercontent.com/nickwild93/cookbook/main/TorrServer/torrserver.service -O /etc/systemd/system/
 systemctl daemon-reload
 systemctl start torrserver
 systemctl enable torrserver
