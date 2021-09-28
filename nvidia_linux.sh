7c5b4dce8809f884c98f22ac4870302bd7bbc511
nickwild93 <91117443+nickwild93@users.noreply.github.com>
2021-09-28 13:55:16 +0300

    Create nvidia_linux.sh

---
+++ nvidia_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://ru.download.nvidia.com/XFree86/Linux-x86_64/340.108/NVIDIA-Linux-x86_64-340.108.run
+chmod +x NVIDIA-Linux-x86_64-340.108.run
+./NVIDIA-Linux-x86_64-340.108.run
